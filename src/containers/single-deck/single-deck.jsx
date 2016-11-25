8407920125cd7f3ae73ca1923118e6e8d9adb6d5
naughtyfiddle <ctdinh93@gmail.com>
2016-11-25 02:19:14 -0600

    Add container skeletons

---
+++ src/containers/single-deck/single-deck.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Card from '../../shared/card.jsx';
+
+export default class SingleDeck extends React.Component {
+
+	render() {
+		return(
+			<div className="single-deck">
+				Single deck mode
+				<Card imgUrl="img/planes/2009/bant.jpg"/>
+			</div>
+		);
+	}
+}
