1b0bfc012708522e29771a47ccb9bf80f15b1607
naughtyfiddle <ctdinh93@gmail.com>
2016-11-25 11:51:03 -0600

    Route to eternities map

--- src/app.jsx
+++ src/app.jsx
@@ -2,21 +2,32 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, Link, browserHistory } from 'react-router';
 
+import EternitiesMap from './containers/eternities-map/eternities-map.jsx';
 import SingleDeck from './containers/single-deck/single-deck.jsx';
 
 const App = (props) => {
 	return (
 		<div>
 			App
-			<Link to="single-deck">link</Link>
+			<Link to="single-deck">
+				Single Deck
+			</Link>
+			<Link to="eternities-map">
+				Eternities Map
+			</Link>
 			{props.children}
 		</div>
 	);
 };
 
+App.propTypes = {
+	children: React.PropTypes.object
+};
+
 ReactDOM.render((
 	<Router history={browserHistory}>
 		<Route path="/" component={App}>
+			<Route path="eternities-map" component={EternitiesMap}/>
 			<Route path="single-deck" component={SingleDeck}/>
 		</Route>
 	</Router>
--- src/containers/single-deck/single-deck.jsx
+++ src/containers/single-deck/single-deck.jsx
@@ -12,3 +12,4 @@ export default class SingleDeck extends React.Component {
 		);
 	}
 }
+
