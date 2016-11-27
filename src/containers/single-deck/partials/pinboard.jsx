379d0d3f6fd4a738a02649c7a042f4562e5a9607
naughtyfiddle <ctdinh93@gmail.com>
2016-11-27 02:12:33 -0600

    Make pinning and unpinning cards prettier, change app theme colors

--- src/containers/single-deck/partials/pinboard.jsx
+++ src/containers/single-deck/partials/pinboard.jsx
@@ -1,20 +1,35 @@
 import React from 'react';
 import _ from 'lodash';
 
+import MaterialIcon from '../../../shared/material-icon.jsx';
+
 export default function Pinboard(props) {
 	const pinnedCards = _.map(props.cards, (card) => {
 		return (
-			<div onClick={_.partial(props.unpin, card)}>
+			<div className="pinned-card">
+				<button
+					className="unpin-card"
+					onClick={_.partial(props.unpin, card)}
+				>
+					<MaterialIcon name="remove" />
+				</button>
 				{card}
 			</div>
 		);
 	});
 
-	return (
+	const pinboard = (
 		<div className="pinboard">
-			{pinnedCards}
+			<div className="header">
+				Pinned Cards
+			</div>
+			<div className="pinned-cards">
+				{pinnedCards}
+			</div>
 		</div>
 	);
+
+	return props.cards.length > 0 ? pinboard : null;
 }
 
 Pinboard.propTypes = {
