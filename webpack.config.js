af185fc98b780c1c37998b6540b187906a597175
naughtyfiddle <ctdinh93@gmail.com>
2018-10-25 01:52:02 -0500

    make build more better

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,16 @@
 const path = require('path');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 const config = {
 	stats: 'normal',
-	entry: {
-		js: path.resolve(__dirname, './src/index.jsx'),
-		css: path.resolve(__dirname, './src/main.less')
-	},
+	entry: path.resolve(__dirname, './src/index.jsx'),
 	output: {
 		path: path.resolve(__dirname, './dist'),
 		// unfortunately we end up with a useless css-bundle.js
-		filename: '[name]-bundle.js'
+		filename: 'bundle.js'
 	},
 	module: {
 		rules: [{
@@ -21,28 +21,44 @@ const config = {
 		}, {
 			// compile less to css, remove webpack runtime, and output to css/bundle.css
 			use: [
-				'file-loader?name=css-bundle.css',
-				'extract-loader',
+				MiniCssExtractPlugin.loader,
 				'css-loader?url=false', // by default css-loader treats url() like @import for some reason
 				'less-loader'
 			],
 			test: /\.(less|css)$/
 		}]
 	},
+	plugins: [
+		new CopyWebpackPlugin([{
+			from: 'static/img',
+			to: 'img',
+			toType: 'dir',
+			ignore: 'favicon.ico'
+		}]),
+		new MiniCssExtractPlugin({
+			filename: '[name].css'
+		}),
+		new HtmlWebpackPlugin({
+			filename: 'index.html',
+			template: 'static/index.html',
+			favicon: 'static/img/favicon.ico',
+			inject: false
+		})
+	],
 	resolve: {
 		extensions: ['.js', '.jsx']
 	}
 };
 
 module.exports = function(env, options) {
-	if (options.mode === 'development') {
-		config.plugins = [
+	if (options.mode === 'production') {
+		config.plugins.push([
 			new BundleAnalyzerPlugin({
 				reportFilename: '../bundle-report.html',
 				analyzerMode: 'static',
 				openAnalyzer: false
 			})
-		];
+		]);
 	}
 	return config;
 };
